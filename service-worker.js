2b2c612272acc945f76e53742cd09e6a1e6bb513
Tu Nombre <sasogu@gmail.com>
2025-04-01 15:27:27 +0200

    funciona!

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE_NAME = "pwa-cszcm-v7.1.6";
+const CACHE_NAME = "pwa-cszcm-v7.1.7";
 const OFFLINE_URLS = 
 [
   '/app/inicio/',
