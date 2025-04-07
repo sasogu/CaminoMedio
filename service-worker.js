7656eb9e90d4cd9f1a261b06274ab605c3b999d0
Tu Nombre <sasogu@gmail.com>
2025-04-07 08:59:03 +0200

    version

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE_NAME = "pwa-cszcm-v7.2.5";
+const CACHE_NAME = "pwa-cszcm-v7.2.6";
 const OFFLINE_URLS = 
 [
   '/app/inicio/',
