d23aa00b7cf9d6a57d4d2abfeb29b0175eca74c3
Chuwi <chuwi@adsf.es>
2025-06-05 08:47:58 +0200

    27

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@
 
 importScripts('https://storage.googleapis.com/workbox-cdn/releases/5.1.2/workbox-sw.js');
 
-const CACHE_NAME = "pwa-cszcm-v7.2.26";
+const CACHE_NAME = "pwa-cszcm-v7.2.27";
 const OFFLINE_URLS = [
   // Páginas principales
   '/app/inicio/',
