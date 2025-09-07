dde3a299b9651562dfd8a64ff26869e3ab49c7ed
Chuwi <chuwi@adsf.es>
2025-09-07 22:09:34 +0200

    oscuro

--- service-worker.js
+++ service-worker.js
@@ -1,7 +1,7 @@
 // PWA Service Worker (scope auto-resolves relative to script location)
 
-const SW_VERSION = "0.4.4";
-const CACHE_NAME = "pwa-cszcm-v0.4.4";
+const SW_VERSION = "0.4.5";
+const CACHE_NAME = "pwa-cszcm-v0.4.5";
 // Precarga mínima necesaria para arrancar offline (rutas relativas al scope)
 const OFFLINE_URLS = [
   // Núcleo app
